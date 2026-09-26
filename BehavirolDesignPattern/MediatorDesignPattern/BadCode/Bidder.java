3a123543b73b026c400c1c85117a3e9e9db834bf
Harsh Pandey <harshbpandey5@gmail.com>
2026-09-26 17:32:24 +0530

    add StateDesignPattern

--- BehavirolDesignPattern/MediatorDesignPattern/BadCode/Bidder.java
+++ BehavirolDesignPattern/MediatorDesignPattern/BadCode/Bidder.java
@@ -2,13 +2,11 @@ package BehavirolDesignPattern.MediatorDesignPattern.BadCode;
 
 public class Bidder {
     private String name;
-    private int bid;
     public Bidder(String name){
         this.name = name;
     }
 
     public void placeBid(int amount,Bidder[] bidders){
-        this.bid = amount;
         System.out.println(name + " placed a bid of " + amount);
         for(Bidder bidder : bidders){
             if(bidder != this){
